0b023f717555fbdb69b14d5802e012ff3449c369
HarideVos <hdevos83@gmail.com>
2024-04-21 20:07:22 -0400

    airline booking - week 4 hw

---
+++ AirlineBookingSystem/src/com/homeworkWeek3/Flight.java
@@ -0,0 +1,50 @@
+package com.homeworkWeek3;
+
+
+public class Flight {
+    private String flightNumber;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private int availableSeats;
+
+    public Flight(String flightNumber, String origin, String destination, String departureTime, int availableSeats) {
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.availableSeats = availableSeats;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void bookSeat() {
+        if (availableSeats > 0) {
+            availableSeats--;
+            System.out.println("Seat booked successfully!");
+        } else {
+            System.out.println("No available seats for this flight.");
+        }
+    }
+}
+
+
+
